9944ceac6da2d562b10a30a861653a38fad0b87c
Fatima Rasheed <fatimarasheedch@gmail.com>
2026-03-24 05:03:04 -0700

    add

--- chat-app/frontend/src/app/services/chatApi.js
+++ chat-app/frontend/src/app/services/chatApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const chatApi = createApi({
   reducerPath: "chatApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://week4-day4chatapp.vercel.app" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: (builder) => ({
     getMessages: builder.query({
       query: () => "/messages",
